a78553e1a25c544d9d29ad5c860b0f5e16435fbe
Natalia <natalianekrashevich@gmail.com>
2017-06-04 14:54:31 +0300

    v1.1

---
+++ Lab7_2/Lab7_2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lab7_2
+//
+//  Created by Admin on 23.05.17.
+//  Copyright (c) 2017 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lab7_2/Lab7_2/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  Lab7_2
+//
+//  Created by Admin on 23.05.17.
+//  Copyright (c) 2017 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface GameViewController : GLKViewController
+
+@end
